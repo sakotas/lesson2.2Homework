cb3903c64017ed88015cf98c08c2dd12c58ebd0f
Alex Kotishevskiy <alexkotishevskiy@MacBook-Air-Alex.local>
2017-03-05 18:10:41 +0300

    1st commit

---
+++ Lesson2.2Homework/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Lesson2.2Homework
+//
+//  Created by Alex Kotishevskiy on 05/03/2017.
+//  Copyright © 2017 Alex Kotishevskiy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Lesson2.2Homework/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Lesson2.2Homework
+//
+//  Created by Alex Kotishevskiy on 05/03/2017.
+//  Copyright © 2017 Alex Kotishevskiy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
